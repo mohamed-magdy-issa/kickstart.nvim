f8e6cc9ce41faaec1cf9640111d32dedea284daf
Mohamed Essa <mohamed.essa.ext@orange.com>
2025-01-20 14:03:49 +0200

    Add java lsp

--- init.lua
+++ init.lua
@@ -624,6 +624,9 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
+        java_language_server = {
+          cmd = { '/Users/mohamed-issa/work/lsp/java-language-server/dist/lang_server_mac.sh' },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
