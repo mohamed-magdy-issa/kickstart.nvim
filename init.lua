d01b6218a70836c65d6c1c0b5b2cd406ea91981d
Mohamed Essa <mohamed.essa.ext@orange.com>
2025-02-09 14:44:24 +0200

    Make default telescope seach style a drop down list

--- init.lua
+++ init.lua
@@ -388,11 +388,9 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = require('telescope.themes').get_dropdown {
+          path_display = { 'smart' }, -- Abbreviates paths intelligently
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
